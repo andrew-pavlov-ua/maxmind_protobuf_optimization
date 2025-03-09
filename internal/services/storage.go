81c46a003f9547e028cb656cee3edc0018852951
andrew-pavlov-ua <pavlovandreew@gmail.com>
2025-03-09 22:51:32 +0200

    fix

--- internal/services/storage.go
+++ internal/services/storage.go
@@ -3,7 +3,6 @@ package services
 import (
 	"encoding/json"
 	"fmt"
-	"net"
 	"os"
 
 	"cmd/internal/models"
@@ -103,17 +102,6 @@ func ConvertJSONToProtoFiles(pathJSON string, pathProto string) error {
 	}
 
 	return nil
-}
-
-func LookUpProtoIp(ip net.IP, data *models.Root) (*models.Geo, error) {
-	dataId := data.CidrCountryPairs[ip.String()]
-
-	result := data.Geos[dataId]
-	if result == nil {
-		return nil, fmt.Errorf("not found data by index - ip: %v - %v", dataId, ip)
-	}
-
-	return result, nil
 }
 
 func LookUpProtoCidr(cidr string, data *models.Root) (*models.Geo, error) {
